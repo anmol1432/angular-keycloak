b71bab6490109c05e858d15b73a13ca0f8d3c322
Anmol Singh <anmolbuttar143205@gmail.com>
2022-04-28 17:58:57 +0530

    add getTotal functionnality

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 
@@ -13,13 +14,31 @@ export class DashboardComponent implements OnInit {
     amount: '',
     describe: '',
   });
+  
+  bills:Array<{id:string, describe:string,amount:number}> =[]
 
-  bills:Array<{id:number, describe:string,amount:number}> =[]
+   getUniqueId(parts: number): string {
+    const stringArr = [];
+    for(let i = 0; i< parts; i++){
+      const S4 = (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+      stringArr.push(S4);
+    }
+    return stringArr.join('-');
+  }
+  
   addBill(e: any): void {
     e.preventDefault();
-    console.log('--------->itself', this.billForm.value);
-    this.bills.push(this.billForm.value);
-    console.log("the bills ",this.bills)
+    this.bills.push({...this.billForm.value,id:this.getUniqueId(4)});
+    this.billForm.reset()
+  }
+  total=0;
+  getTotal():any {
+    this.total=0;
+     this.bills.map((item):number=>{
+      this.total= this.total + item.amount;
+      return this.total;
+    })
+    return this.total;
   }
   constructor(private formBuilder: FormBuilder) { }
 
