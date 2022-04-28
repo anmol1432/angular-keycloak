c32f432b754e2be211ef4e6827f6685d435d1bc1
Anmol Singh <anmolbuttar143205@gmail.com>
2022-04-28 16:40:47 +0530

    adding state mangement

---
+++ src/store/actions/course.action.ts
@@ -0,0 +1,17 @@
+import { Action } from '@ngrx/store';
+import { CourseItem } from '../models/courseItem.model';
+
+export enum CourseActionType {
+  ADD_ITEM = '[COURSE] Add Course',
+}
+
+export class AddItemAction implements Action {
+  readonly type = CourseActionType.ADD_ITEM;
+  
+  //add an optional payload
+  constructor(public payload: CourseItem) {
+    console.log("sdasdasd", CourseActionType.ADD_ITEM);
+  }
+}
+
+export type CourseAction = AddItemAction;
