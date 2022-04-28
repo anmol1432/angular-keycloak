21710f23a6c01e1c31447f2346daa70f12bb065b
Anmol Singh <anmol.singh@kochartech.com>
2022-04-28 11:52:11 +0530

    login implemented

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,19 +8,31 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  signInForm =this.formBuilder.group({
-    firstName: '',
-    lastName: '',
-  }); 
+  signInForm = this.formBuilder.group({
+    userName: '',
+    password: '',
+  });
+  isAuthent: boolean = false;
+  errorMsg: string = 'Please enter credentials !';
 
   onSubmit(e: any): void {
-    // e.stopPropagation();
     e.preventDefault();
-    console.log('okay');
+    console.log('--------->', this.signInForm);
+    if (
+      this.signInForm.value.userName == 'Anm1234' &&
+      this.signInForm.value.password == '1234'
+    ) {
+      console.log('login successful');
+      this.isAuthent = false;
+      this.router.navigate(['dashboard']);
+    } else {
+      console.log('Login again failed');
+      this.isAuthent = true;
+      this.signInForm.reset();
+    }
   }
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {}
 }
