0a86e3bf39060daaf3c5722f15358d2c8a1d1dab
Anmol Singh <anmol.singh@kochartech.com>
2022-04-28 11:52:41 +0530

    added dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // pages imports
 import { LoginComponent } from "./login/login.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent},
   {path: '**',redirectTo:'login'}
 ];
 
